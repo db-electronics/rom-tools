9adc71d527e8818c49b43f587ffcde2853a71fd8
René Richard <rene@db-electronics.ca>
2020-01-08 15:21:09 -0500

    added a few parsers arguments

--- src/rom-tools.py
+++ src/rom-tools.py
@@ -8,11 +8,27 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="rom-tools 0.0.0.1")
 
-    parser.add_argument("--ifile",
+    parser.add_argument("-i"
+                        "--ifile",
                         help="the input rom file",
                         nargs=1,
                         type=str,
                         required=True
                         )
 
+    parser.add_argument("-o"
+                        "--ofile",
+                        help="the output rom file",
+                        nargs=1,
+                        type=str,
+                        default="out.bin"
+                        )
+
+    parser.add_argument("--end",
+                        help="swap the endianness of the input file",
+                        action="store_true"
+                        )
+
     args = parser.parse_args()
+
+
